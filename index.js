4c76208914f35aa48ceecba57d9f28ab72329c2e
Devananda Binu <deva31807@gmail.com>
2024-06-05 16:56:45 +0100

    Created playRound() and playGame() function. Also added a return statement in getHumanChoice().

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const objects = ["rock", "paper", "scissors"]
 function getComputedChoice() {
     const computerChoice = objects[(Math.floor(Math.random() * objects.length))];
-    return compChoice;
+    return computerChoice;
 };
 
 function getHumanChoice() {
     const humanChoice = prompt("Choose one: rock, paper or scissors");
     if(objects.includes(humanChoice.toLowerCase())) {
         alert("okay, now the computer's turn!");
+        return humanChoice;
     } else {
         alert("mm well don't type random stuff :(");
         getHumanChoice();
@@ -16,3 +17,26 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound() {
+    if(getComputedChoice() == getHumanChoice()) {
+        alert("Tie!");
+        console.log("Tie");
+    } else if(getComputedChoice() == objects[0] && getHumanChoice() == objects[1] || getComputedChoice() == objects[1] && getHumanChoice() == objects[2] || getComputedChoice() == objects[2] && getHumanChoice() == objects[0]) {
+        alert("U get a point!");
+        console.log("U get a point!");
+        humanScore += 1;
+    } else {
+        alert("Computer gets a point!");
+        console.log("Computer gets a point!");
+        computerScore += 1;
+    }
+};
+
+function playGame() {
+    for (i = 0; i < 5; i++) { playRound(i)};
+    console.log(`Your score: ${humanScore}`);
+    console.log(`Computer's score: ${computerScore}`);
+}
+
+playGame();
