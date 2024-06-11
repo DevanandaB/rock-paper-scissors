332a91dc5adf22c1841123cd95912d2e18d4ec72
Devananda Binu <deva31807@gmail.com>
2024-06-11 13:44:45 +0100

    Modified

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const objects = ["rock", "paper", "scissors"];
 function getComputedChoice() {
     const computerChoice = objects[(Math.floor(Math.random() * objects.length))];
     console.log(`computer chose ${computerChoice}`);
+    return computerChoice;
 };
 
 const gameContainer = document.querySelector(".gameContainer");
 const title = document.createElement("h1");
 const rockBtn = document.createElement("button");
-rockBtn.classList.add("btn");
 const paperBtn = document.createElement("button");
 const scissorsBtn = document.createElement("button");
 const result = document.querySelector(".result");
 const para = document.createElement("p");
+const resultPara = document.createElement("p");
 
 title.innerText = "Rock Paper Scissors";
 rockBtn.innerText = "Rock";
@@ -24,38 +25,44 @@ gameContainer.appendChild(rockBtn);
 gameContainer.appendChild(paperBtn);
 gameContainer.appendChild(scissorsBtn);
 gameContainer.appendChild(para);
+gameContainer.appendChild(resultPara);
 
 rockBtn.addEventListener("click", getHumanChoice);
 paperBtn.addEventListener("click", getHumanChoice);
 scissorsBtn.addEventListener("click", getHumanChoice);
 
-function getHumanChoice(event) {
-    let humanChoice = event.target.innerHTML;
-    para.textContent = `You chose ${humanChoice}`;
-    console.log(getComputedChoice());
+// rockBtn.onclick = getHumanChoice;
+// paperBtn.onclick = getHumanChoice;
+// scissorsBtn.onclick =  getHumanChoice;
+
+function getHumanChoice(e) {
+    let humanChoice = e.target.innerText;
+    para.textContent = `You chose ${humanChoice}`;  
+    getComputedChoice();
+    return humanChoice; 
 };
 
 let humanScore = 0;
 let computerScore = 0;
 
-// function playRound(computerChoice, humanChoice) {
-//     if(computerChoice == humanChoice) {
-//         console.log("Tie");
-//     } else if(computerChoice == objects[0] && humanChoice == objects[1] 
-//         || computerChoice == objects[1] && getHumanChoice == objects[2] 
-//         || computerChoice == objects[2] && humanChoice == objects[0]) {
-//         console.log("U get a point!");
-//         humanScore += 1;
-//     } else {
-//         console.log("Computer gets a point!");
-//         computerScore += 1;
-//     }
-// };
+function playRound() {
+    if(getHumanChoice === getComputedChoice) {
+        resultPara.textContent = "Tie";
+    } else if(getComputedChoice == objects[0] && getHumanChoice == objects[1] 
+        || getComputedChoice == objects[1] && getHumanChoice == objects[2] 
+        || getComputedChoice == objects[2] && getHumanChoice == objects[0]) {
+        resultPara.textContent = "U get a point!";
+        humanScore += 1;
+    } else {
+        resultPara.textContent = "computer gets a point!";
+        computerScore += 1;
+    }
+};
 
-// function playGame() {
-//     for (i = 0; i < 5; i++) { playRound(getComputedChoice(), getHumanChoice())};
-//     console.log(`Your score: ${humanScore}`);
-//     console.log(`Computer's score: ${computerScore}`);
-// }
+function playGame() {
+    for (i = 0; i < 5; i++) { playRound(getComputedChoice(), getHumanChoice())};
+    console.log(`Your score: ${humanScore}`);
+    console.log(`Computer's score: ${computerScore}`);
+}
 
-// playGame();
+playGame();
